f23865a6eaa79936ea8f00f3813865c4f7ae0fd3
kira_eleni <C:\Users\user2345\AppData\Roaming\The Bat!>
2016-04-11 17:20:44 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿var http = require('http');//http - вбудований модуль nodejs
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write( (new Date()).toLocaleTimeString() );
+  res.end();
+}).listen(8080); //порт
+
+console.log('Server running on port 8080');
